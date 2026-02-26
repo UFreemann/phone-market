c897ab6daa2c51686c1fc348e9866a54b662fc16
Senyo Ebenezer <senyoebenezer8@gmail.com>
2026-02-26 22:34:56 +0000

    Final Launch Version

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/db';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap() {
   const products = await prisma.product.findMany({
     select: { id: true, updatedAt: true },
